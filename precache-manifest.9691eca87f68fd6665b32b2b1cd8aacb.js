056a072175a73b86a494e8ce8495ec231c2047a9
linkzhang <1519815799@qq.com>
2023-12-27 13:05:02 +0800

    deploy

---
+++ precache-manifest.9691eca87f68fd6665b32b2b1cd8aacb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b119735f389cddc2dd11",
+    "url": "/morney-1-website/css/app.901860a1.css"
+  },
+  {
+    "revision": "8d751debaedbc23baf590494b2ccada1",
+    "url": "/morney-1-website/index.html"
+  },
+  {
+    "revision": "b119735f389cddc2dd11",
+    "url": "/morney-1-website/js/app.1ba9faa9.js"
+  },
+  {
+    "revision": "7c35ba610420328a1486",
+    "url": "/morney-1-website/js/chunk-vendors.833dd7bc.js"
+  },
+  {
+    "revision": "6576646f5c9ff69f4a17734836f662b5",
+    "url": "/morney-1-website/manifest.json"
+  },
+  {
+    "revision": "fcd1ba7e10c055653f258d3785941ea1",
+    "url": "/morney-1-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-1-website/robots.txt"
+  }
+]);
